ecf9a62f0d1b317b019fb385e8e450b071f39029
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-18 14:23:09 +0530

    create Welcome Green Hill section in Home page

--- components/Home/WellcomeToGrennHill.tsx
+++ components/Home/WellcomeToGrennHill.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const WellcomeToGrennHill = () => {
   return (
-    <div>WellcomeToGrennHill</div>
+    <>
+        <div className='bg-[#1a1919]  py-28'>
+            <div className="text-center  ">
+                <p className="text-[4.125rem] font-['Playfair_Display']">Welcome to Simon Greenhill</p>
+                <hr className="w-[15%] mx-auto mt-2 border-2 rounded-2xl border-[rgba(201,172,140,1)] "/>
+            </div>
+            
+            <div className="text-center text-[rgba(201,172,140,1)] italic mt-10 text-[1.2rem]">
+                Greenhill events started life in 2007. We offer an all-inclusive event service. 
+                From weddings to birthday parties, Corporate Events to Christmas parties,
+                <span>
+                Match Day Entertainment to Private Parties we pride ourselves on delivering bespoke event solutions.</span>
+            </div>
+        </div>
+    </>
   )
 }
 
