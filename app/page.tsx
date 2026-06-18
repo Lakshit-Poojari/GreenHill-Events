c285adde0477a659972d236db084e107b94c845b
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-18 12:18:19 +0530

    Home page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import { Companys } from "@/components/Home/Companys";
+import DistinctOffering from "@/components/Home/DistinctOffering";
+import HeroSection from "@/components/Home/HeroSection";
+import RecentPosts from "@/components/Home/RecentPosts";
+import Testimonials from "@/components/Home/Testimonials";
+import WellcomeToGrennHill from "@/components/Home/WellcomeToGrennHill";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      
+      <div className="px-23">
+        <HeroSection/>
+        <WellcomeToGrennHill/>
+        <DistinctOffering/>
+        <Testimonials/>
+        <RecentPosts/>
+        <Companys/>
+      </div>
     </>
   );
 }
