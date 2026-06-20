26312232ef75ed17b77de2566138408bbeabb269
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-20 17:47:49 +0530

    Event section

---
+++ components/Event/Heading.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <>
+        <div>
+          <p className="font-['Poppins'] text-[5.5rem] font-light text-center text-white">
+            Events
+          </p>
+          <hr className="w-[12%] mx-auto mt-2 border-2 rounded-full border-[#C9AC8C]" />
+        </div>
+    </>
+  )
+}
+
+export default Heading
