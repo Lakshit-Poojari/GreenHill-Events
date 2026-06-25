c537625268d86ba2d020bab12e431738676627f3
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-25 16:34:54 +0530

    create Entertainment resuaable card

---
+++ app/(pages)/entertainment/(Entertainment)/cabaret-singing-waiters/page.tsx
@@ -0,0 +1,44 @@
+import EntertainmentCards from '@/components/Entertainment/EntertainmentCards';
+import SectionHeading from '@/components/SectionHeading'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const cards = [
+  {
+    title: "Singing Waiters",
+    title1:'West End Waiters',
+    image: "/Entertainment/Cabaret-waiter/WEST-END-WAITERS.jpg",
+    text: "Our West End stars perform as fabulous operatic waiters: the fantastic music, the unforgettable characters and their remarkable performances will leave your guests cheering for more. And with our West End Waiters, there is more…a stunning 45minute cabaret set is sure to delight your guests.",
+  },
+  {
+    title: "Pop Opera",
+    title1:'Pop Up Tenors',
+    image: "/Entertainment/Cabaret-waiter/POP-UP-TENORS.jpg",
+    text: "Pop Up Tenors can surprise your audiences as waiters, or be saved as the ultimate after dinner treat; either way, with inch perfect choreography and harmonies to die for – the boys are guaranteed to have your guests dancing.",
+  },
+  {
+    title: "Singing Waiters",
+    title1:'Operatic Singing Waiters',
+    image: "/Entertainment/Cabaret-waiter/OPERATIC-SINGING-WAITERS.jpg",
+    text: "Our Singing Waiters have performed in over thirty countries, from intimate gatherings to large award ceremonies and society weddings. We take immense pride in hand picking our performers direct from London’s West End.",
+  },
+  {
+    title: "Singing Waiters",
+    title1:'Female Leading Ladies',
+    image: "/Entertainment/Cabaret-waiter/Leading-Ladies.jpg",
+    text: `Some of the West End’s finest performers serve up the ultimate cabaret as our Leading Ladies. Our ladies will transport your guests to the glittering heights of theatre land and beyond in this jaw dropping show.`,
+  },
+];
+
+const page = () => {
+  return (
+    <>
+        <EntertainmentCards heading="CABARET & SINGING WAITERS"
+        description="Cabaret and Singing waiters can bring an element of theatre and surprise to any event."
+        cards={cards}/>
+    </>
+  )
+}
+
+export default page
---
+++ components/Entertainment/EntertainmentCards.tsx
@@ -0,0 +1,93 @@
+import Image from "next/image";
+import Link from "next/link";
+import SectionHeading from "@/components/SectionHeading";
+
+interface Card {
+  title: string;
+  title1: string;
+  image: string;
+  text: string;
+  link?: string;
+}
+
+interface Props {
+  heading: string;
+  description: string;
+  cards: Card[];
+}
+
+const EntertainmentCards = ({
+  heading,
+  description,
+  cards,
+}: Props) => {
+  return (
+    <div className="pt-26 mx-16.25 px-6.75">
+      <SectionHeading title={heading} />
+
+      <div className="text-[#C9AC8C] text-lg italic text-center space-y-1.5 py-4 font-['Old_Standard_TT']">
+        <p>{description}</p>
+      </div>
+
+      <div className="bg-[#686868] py-6">
+        <div className="mx-4 md:mx-8 lg:mx-16.25 px-4 md:px-6.75 py-10">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            {cards.map((card, index) => (
+              <div
+                key={index}
+                className={`group w-full h-112.5 perspective-[1000px]
+                  ${
+                    cards.length % 2 !== 0 &&
+                    index === cards.length - 1
+                      ? "lg:col-span-2 lg:max-w-[50%] lg:mx-auto"
+                      : ""
+                  }`}
+              >
+                <div className="relative h-full w-full transition-transform duration-700 transform-3d group-hover:transform-[rotateY(180deg)]">
+
+                  {/* Front */}
+                  <div className="absolute inset-0 backface-hidden">
+                    <Image
+                      src={card.image}
+                      fill
+                      alt={card.title1}
+                      className="object-cover object-left rounded-lg brightness-35"
+                    />
+                  </div>
+
+                  <div className="absolute backface-hidden w-3xl top-40 left-1/2 -translate-x-1/2">
+                    <h3 className="text-white text-3xl font-bold text-center">
+                      {card.title}
+                    </h3>
+
+                    <p className="text-white text-2xl font-bold text-center my-2">
+                      {card.title1}
+                    </p>
+
+                    <hr className="w-25.5 mx-auto border-2 border-[#C9AC8C]" />
+                  </div>
+
+                  {/* Back */}
+                  <div className="absolute inset-0 bg-black rounded-lg p-6 flex flex-col justify-between transform-[rotateY(180deg)] backface-hidden">
+                    <p className="text-[#C9AC8C]">
+                      {card.text}
+                    </p>
+
+                    <Link href={card.link || "#"} className="self-center">
+                      <button className="px-6 py-3 border border-[#C9AC8C] rounded-full text-[#C9AC8C] hover:bg-[#C9AC8C] hover:text-black">
+                        FIND OUT MORE
+                      </button>
+                    </Link>
+                  </div>
+
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EntertainmentCards;
