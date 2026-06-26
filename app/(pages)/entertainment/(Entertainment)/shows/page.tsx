50a3736c1fc70dec3710ed6a00aaae9f0d6bd72b
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-26 17:21:14 +0530

    create shows page

---
+++ app/(pages)/entertainment/(Entertainment)/shows/page.tsx
@@ -0,0 +1,30 @@
+import EntertainmentCards from '@/components/Entertainment/EntertainmentCards'
+import React from 'react'
+
+const cards = [
+  {
+    title: "show",
+    title1:'Slam',
+    image: "/Entertainment/Shows/Slam.png",
+    text: "The Slam! boys have been touring their show for the last 8 years all over the world. From motor shows in Korea to international conferences at Euro Disney, this hard-hitting percussion show is a top-notch act that will resonate in the minds of the guests long after the event",
+  },
+  {
+    title: "Show",
+    title1:'Lumina',
+    image: "/Entertainment/Shows/Lumina.png",
+    text: "Lume is a sensational and breathtaking act featuring stunning violin performance and state of the art laser technology. The Laser Violinist literally plays her violin with a laser beam and manipulates lasers during her electrifying show.",
+  },
+
+];
+
+const page = () => {
+  return (
+    <div>
+        <EntertainmentCards heading='Shows' 
+        description='' 
+        cards={cards}/>
+    </div>
+  )
+}
+
+export default page
