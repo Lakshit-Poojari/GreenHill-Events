6b93e8f13dd581db43b0dc75f846360762f57a49
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-01 16:37:03 +0530

    create dashboard

--- app/(admin)/controlpanel/(dashboard)/layout.tsx
+++ app/(admin)/controlpanel/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
-import "../../globals.css";
+import Navbar from "@/components/Admin/Navbar";
+import "../../../globals.css";
 
 export default function DashboardLayout({
   children,
@@ -7,7 +8,11 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      {children}
+      <Navbar />
+
+      <main className="mx-auto max-w-7xl p-6">
+        {children}
+      </main>
     </>
   );
 }
---
+++ components/Admin/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="h-16 border-b bg-#0000">
+      <div className="mx-auto flex h-full max-w-7xl items-center justify-between px-6">
+
+        <h1 className="text-2xl font-bold text-[#C9AC8C]">
+          Green Hill Admin
+        </h1>
+
+        <nav className="flex items-center gap-8">
+
+          <Link href="/controlpanel/dashboard">Dashboard</Link>
+
+
+          <Link href="/controlpanel/posts">Posts</Link>
+
+          <Link href="/controlpanel/comments">Comments</Link>
+
+          {/* Show only if SUPER_ADMIN */}
+          <Link href="/controlpanel/users">Users</Link>
+
+        </nav>
+
+        <div className="flex items-center gap-4">
+
+          <span className="font-medium">
+            Admin
+          </span>
+
+          <button className="rounded-md bg-red-500 px-4 py-2 text-white">
+            Logout
+          </button>
+
+        </div>
+
+      </div>
+    </header>
+  );
+}
