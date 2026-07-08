b25112f8b5bd426fa59ca2801850a3b0ba927dec
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-08 11:03:25 +0530

    create category

---
+++ backend/types/categoryType.ts
@@ -0,0 +1,36 @@
+// types/categoryTypes.ts
+
+export enum CategoryStatus {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+}
+
+export interface Category {
+  id: number;
+  category_name: string;
+  menu_name: string;
+  slug: string;
+  image: string;
+  description: string;
+  status: CategoryStatus;
+  created_by: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface CreateCategoryType {
+  category_name: string;
+  menu_name: string;
+  image: string;
+  description: string;
+  status: CategoryStatus;
+  created_by: number;
+}
+
+export interface UpdateCategoryType {
+  category_name?: string;
+  menu_name?: string;
+  image?: string;
+  description?: string;
+  status?: CategoryStatus;
+}
