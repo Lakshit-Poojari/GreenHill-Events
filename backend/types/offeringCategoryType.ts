4df88a79cb42373f645872cfff35a02821629f28
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-09 15:23:38 +0530

    create offering Category type page

---
+++ backend/types/offeringCategoryType.ts
@@ -0,0 +1,34 @@
+export enum offeringCategoryStatus{
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+}
+
+export interface OfferingCategory {
+    id: number;
+    category_id: number;
+    name: string;
+    slug: string;
+    display_order: number;
+    status: offeringCategoryStatus;
+    created_at: Date;
+    updated_at: Date;
+    updated_by: number | null;
+}
+
+export interface createOfferingCategory{
+    category_id: number;
+    name: string;
+    slug: string;
+    display_order: number;
+    status: offeringCategoryStatus;
+    updated_by?: number;
+}
+
+export interface updateOfferingCategory{
+    category_id?: number;
+    name?: string;
+    slug?: string;
+    display_order?: number;
+    status?: offeringCategoryStatus;
+    updated_by?: number;
+}
