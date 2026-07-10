f1e29653343c005993ce97531e7d77b02c9ca7d1
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-10 11:49:43 +0530

    add createOfferingService and updateOfferingService

--- backend/services/offeringService.ts
+++ backend/services/offeringService.ts
@@ -1,17 +1,50 @@
-import { deleteOfferingModel, getAllOfferingModel, getSingleOfferingModel } from "../models/offeringModel";
+import { createOfferingModel, deleteOfferingModel, getAllOfferingModel, getOfferingBySlugModel, getSingleOfferingModel, updateOfferingModel } from "../models/offeringModel";
+import { CreateOffering, UpdateOffering } from "../types/offeringType";
 
-export async function createOfferingService(){
+export async function createOfferingService(offering:CreateOffering){
     try {
+        const slug = offering.performer_name.toLowerCase().trim().replace(/\s+/g, "-").replace(/[^\w-]+/g, "");
         
+        if (!offering.performer_name || !offering.small_description || !offering.image_path ||
+            !offering.large_description || !offering.status || !offering.offering_category_id
+        ) {
+            throw new Error("All Field Required")
+        }
+
+        const existingOffering = await getOfferingBySlugModel(slug)
+
+        if (existingOffering) {
+            throw new Error("Offering already exists.");
+        }
+        const result = await createOfferingModel({...offering, slug})
+
+        return result
     } catch (error) {
         console.error("Error in Create Offering Service", error);
         throw error;
     }
 }
 
-export async function UpdateOfferingService(){
+export async function updateOfferingService(id:number, offering:UpdateOffering, slug:string){
     try {
-        
+        if (!id || !offering.offering_category_id || !offering.performer_name || !offering.slug ||
+            !offering.image_path || !offering.small_description || !offering.large_description || !offering.status) {
+            throw new Error("All Field Required")
+        }
+
+        const existingOffering = await getSingleOfferingModel(id)
+
+        if(existingOffering){
+            throw new Error("Offering not found.")
+        }
+
+        const duplicateOffering = await getOfferingBySlugModel(slug)
+        if(duplicateOffering) {
+            throw new Error("Offering already exists.");
+        }
+
+        const result = await updateOfferingModel(id, offering)
+        return result
     } catch (error) {
         console.error("Error in Update Offering Service", error);
         throw error;
