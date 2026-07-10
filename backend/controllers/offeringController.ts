36f7207471e8e2e2ce656f019599ded8ff7055f1
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-10 12:56:25 +0530

    create offering controller

---
+++ backend/controllers/offeringController.ts
@@ -0,0 +1,52 @@
+import { createOfferingService, deleteOfferingService, getAllOfferingService, getSingleOfferingService, updateOfferingService } from "../services/offeringService";
+import { CreateOffering, Offering, UpdateOffering } from "../types/offeringType";
+
+export async function createOfferingController(offering:CreateOffering){
+    try {
+        const result = await createOfferingService(offering)
+        return result
+    } catch (error) {
+        console.error("Create Offering Controller Error", error);
+        throw new Error
+    }
+}
+
+export async function updateOfferingController(id:number, offering:UpdateOffering, slug:string){
+    try {
+        const result = await updateOfferingService(id, offering, slug)
+        return result
+    } catch (error) {
+        console.error("Create Offering Controller Error", error);
+        throw new Error
+    }
+}
+
+export async function getAllOfferingController(){
+    try {
+        const result = await getAllOfferingService()
+        return result
+    } catch (error) {
+        console.error("Create Offering Controller Error", error);
+        throw new Error
+    }
+}
+
+export async function getSingleOfferingController(id:number){
+    try {
+        const result = await getSingleOfferingService(id)
+        return result
+    } catch (error) {
+        console.error("Create Offering Controller Error", error);
+        throw new Error
+    }
+}
+
+export async function deleteOfferingController(id:number){
+    try {
+        const result = await deleteOfferingService(id)
+        return result
+    } catch (error) {
+        console.error("Create Offering Controller Error", error);
+        throw new Error
+    }
+}
