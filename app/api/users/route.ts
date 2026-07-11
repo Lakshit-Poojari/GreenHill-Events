afabdf9d89f9f6b5e3063fdc59a0e8332fa5d03c
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-11 13:07:29 +0530

    create user api folder structure

---
+++ app/api/users/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(response:NextResponse){
+    try {
+        // const user = await getAll
+    } catch (error) {
+        
+    }
+}
+
+export async function POST(request:NextRequest){
+    try {
+        // const 
+    } catch (error) {
+        
+    }
+}
