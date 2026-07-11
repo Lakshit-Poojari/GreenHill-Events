693c67eb555e7b304e883fc83f6bda8f01aca9f7
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-11 14:27:47 +0530

    create post method

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,17 +1,47 @@
+import { createUserController, getAllUserController } from "@/backend/controllers/userController";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(response:NextResponse){
+export async function GET(){
     try {
-        // const user = await getAll
+        const users = await getAllUserController()
+        return NextResponse.json({success:true, users}, {status:200})
     } catch (error) {
-        
+        return NextResponse.json(
+            {
+                success:false, 
+                message:error instanceof Error
+                        ? error.message
+                        : "Internal Server Error",
+            },
+            {
+                status: 500,
+            }
+        )
     }
 }
 
 export async function POST(request:NextRequest){
     try {
-        // const 
+        const body = await request.json()
+
+        const result = await createUserController(body)
+
+        return NextResponse.json(
+            {
+                success:true,
+                message:"User created successfull",
+            },
+            {
+                status:201
+            }
+        )
     } catch (error) {
-        
+        return NextResponse.json(
+            {
+                success:false,
+                message: error instanceof Error? error.message : "Internal Error"
+            },
+            {status:400}
+        )
     }
 }
