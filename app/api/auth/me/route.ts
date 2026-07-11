588d36a297327951ab5533decb95b1e74929ff50
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-11 11:09:42 +0530

    create me api

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,20 @@
+import { verifyToken } from "@/backend/middleware/authMiddleware";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    const cookiesStore = await cookies();
+
+    const token = cookiesStore.get("token")?.value
+
+    if (!token) {
+        return NextResponse.json({message:"Unauthorised"}, {status:401})
+    }
+    try {
+        const user = await verifyToken(token)
+
+        return NextResponse.json({success:true, user:user}, {status:200})
+    } catch (error) {
+        return NextResponse.json({success:false, message:"Invalid token"}, {status:401})
+    }
+}
