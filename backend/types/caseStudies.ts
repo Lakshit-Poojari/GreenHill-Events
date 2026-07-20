f748f4993f922040bbe241c2452e6ef84884fa05
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-20 12:36:19 +0530

    create case Studies types

---
+++ backend/types/caseStudies.ts
@@ -0,0 +1,38 @@
+export enum CaseStudyStatus {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+}
+
+export interface CaseStudy {
+  id: number;
+  title: string;
+  slug: string;
+  image: string;
+  description: string;
+  youtube_url: string | null;
+  status: CaseStudyStatus;
+
+  created_by: number | null;
+  created_at: string;
+
+  updated_by: number | null;
+  updated_at: string;
+}
+
+export interface CreateCaseStudy {
+  title: string;
+  slug: string;
+  image: File | string;
+  description: string;
+  youtube_url?: string;
+  status: CaseStudyStatus;
+}
+
+export interface UpdateCaseStudy {
+  title?: string;
+  slug?: string;
+  image?: File | string;
+  description?: string;
+  youtube_url?: string;
+  status?: CaseStudyStatus;
+}
