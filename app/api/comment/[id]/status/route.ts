5da32938f8c2952143e08607ae6a61b278e8760c
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-28 12:45:17 +0530

    create status change api

---
+++ app/api/comment/[id]/status/route.ts
@@ -0,0 +1,46 @@
+import { updateCommentStatusController } from "@/backend/controllers/commentsController";
+import { verifyToken } from "@/backend/middleware/authMiddleware";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const token = request.cookies.get("token")?.value;
+
+    if (!token) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Unauthorized",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+
+    const user = verifyToken(token);
+    const { id } = await params;
+    const body = await request.json();
+
+    await updateCommentStatusController(Number(id), {
+      ...body,
+      approved_by: user.id,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          error instanceof Error
+            ? error.message
+            : "Failed to update the status of comment.",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
