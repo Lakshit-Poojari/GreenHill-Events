ae7b3ae9a0ce7776715fe879001bf63431d22a32
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-30 13:03:04 +0530

    add more functionality in model

--- backend/models/contactModel.ts
+++ backend/models/contactModel.ts
@@ -1,6 +1,6 @@
-import { ResultSetHeader } from "mysql2";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 import db from "../lib/db";
-import { CreateContactType } from "../types/contactType";
+import { Contact, CreateContactType } from "../types/contactType";
 
 export async function createContactModel(contactEmail: CreateContactType) {
   try {
@@ -18,12 +18,53 @@ export async function createContactModel(contactEmail: CreateContactType) {
         contactEmail.email,
         contactEmail.phone || null,
         contactEmail.message,
-      ]
+      ],
     );
 
     return result;
   } catch (error) {
     console.error("Create Contact Model Error:", error);
+    throw error;
+  }
+}
+
+type ContactRow = Contact & RowDataPacket;
+export async function getAllContactModel(page: number, limit: number) {
+  try {
+    const offset = (page - 1) * limit;
+
+    const [result] = await db.query<ContactRow[]>(
+      `
+      SELECT *
+      FROM contacts
+      ORDER BY created_at DESC
+      LIMIT ? OFFSET ?
+      `,
+      [limit, offset],
+    );
+
+    return result;
+  } catch (error) {
+    console.error("Get All Contact Model Error:", error);
+    throw error;
+  }
+}
+
+export async function getSingleContactModel(id: number) {
+  try {
+    const [result] = await db.query<ContactRow[]>(
+      `
+      SELECT *
+      FROM contacts
+      WHERE id = ?
+      LIMIT 1
+      `,
+      [id],
+    );
+
+    return result[0];
+  } catch (error) {
+    console.error("Get Single Contact Model Error:", error);
     throw error;
   }
 }
