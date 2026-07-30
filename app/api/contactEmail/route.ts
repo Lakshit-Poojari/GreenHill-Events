e16294dee865db82efc0abb457e7fb7178f2a0d2
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-07-30 13:33:48 +0530

    create pagination

--- app/api/contactEmail/route.ts
+++ app/api/contactEmail/route.ts
@@ -1,4 +1,4 @@
-import { createContactcontroller } from "@/backend/controllers/contactController";
+import { createContactcontroller, getAllContactController } from "@/backend/controllers/contactController";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -15,6 +15,38 @@ export async function POST(request: NextRequest) {
         status: 200,
       },
     );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          error instanceof Error ? error.message : "Internal server error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const page = Number(searchParams.get("page")) || 1;
+    const limit = Number(searchParams.get("limit")) || 10;
+
+    const contact = await getAllContactController(page, limit);
+
+    return NextResponse.json(
+        {
+            success:true,
+            contact
+        },
+        {
+            status:200
+        }
+    )
   } catch (error) {
     return NextResponse.json(
       {
